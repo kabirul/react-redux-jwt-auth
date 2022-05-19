0d4443f78e5802f1b902b77270df6cfc270cbe98
Kabirul Islam Mondal <kabirul05@gmail.com>
2022-05-19 09:33:37 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+
+export default class Footer extends Component {
+
+  render() {    
+    return (      
+        <div className="container mt-3">
+		   <p className="text-center">React redux jwt auth tutorial  <a href="https://github.com/kabirul" target="_blank" rel="noopener noreferrer">amica.</a></p>
+		</div>
+    );
+  }
+}
